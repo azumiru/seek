aef1359df282bd49af753958a51a8d332d631d83
abduaz <azmir.abdul.ghaffar@f-secure.com>
2017-09-14 03:07:30 +0800

    update js

--- js/script.js
+++ js/script.js
@@ -1,49 +1,52 @@
 
-var defaultPricing = {
-  'classic': 269.99,
-  'standout': 322.99,
-  'premium': 394.99
+var pricing = {
+  'default': {
+    'classic': 269.99,
+    'standout': 322.99,
+    'premium': 394.99
+  },
+  'unilever': {
+    'classic': 269.99,
+    'standout': 322.99,
+    'premium': 394.99
+  },
+  'apple': {
+    'classic': 269.99,
+    'standout': 322.99,
+    'premium': 394.99
+  },
+  'nike': {
+    'classic': 269.99,
+    'standout': 322.99,
+    'premium': 394.99
+  }
 }
 
-var unileverPricing = {
-  'classic': 269.99,
-  'standout': 322.99,
-  'premium': 394.99
-}
+function checkout(cust) {
+  var totalAd1 = document.getElementById('ad1').value;
+  var totalAd2 = document.getElementById('ad2').value;
+  var totalAd3 = document.getElementById('ad3').value;
 
-var applePricing = {
-  'classic': 269.99,
-  'standout': 322.99,
-  'premium': 394.99
-}
-
-var nikePricing = {
-  'classic': 269.99,
-  'standout': 322.99,
-  'premium': 394.99
-}
-
-function checkout(price) {
-  this.firstAd = price['classic'];
-  this.secondAd = price['standout'];
-  this.thirdAd = price['premium'];
-
-  this.add = function() {
-    this.firstAd + this.secondAd + this.thirdAd;
+  switch (cust) {
+    case 'unilever':
+      this.firstAd = pricing.unilever.classic;
+      this.secondAd = pricing.default.standout;
+      this.thirdAd = pricing.default.premium;
+      break;
+    default:
+      this.firstAd = pricing.default.classic;
+      this.secondAd = pricing.default.standout;
+      this.thirdAd = pricing.default.premium;
   }
 
   this.total = function() {
-    var totalAd1 = document.getElementById('ad1');
-    var totalAd2 = document.getElementById('ad2');
-    var totalAd3 = document.getElementById('ad3');
-
     return (this.firstAd * totalAd1)  + (this.secondAd * totalAd2) + (this.thirdAd * totalAd3);
   }
 }
 
 function calc() {
-  var co = new checkout(defaultPricing);
-  document.getElementById('total').innerHTML = '$' + co.total();
+  var co = new checkout('default');
+  document.getElementById('total').innerHTML = '$' + parseFloat(Math.round(co.total() * 100) / 100).toFixed(2);
 }
 
 function init() {
